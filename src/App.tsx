36cef49d0e641813129680e6cca1a92d9633071f
Alan Teles <alan.teles12@outlook.com>
2022-02-09 00:06:41 -0300

    Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './general.css';
+export default function App() {
+	const [fibonacci, setFibonacci] = useState<number>(0);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	function generateSequenceFibonacci(limit: number): number[]{
+		let arr  = [0, 1];
+		for(let i = 1; i < limit; i++){ arr.push(arr[i - 1] + arr[i]); }
+		return arr;
+	}
+	return (
+		<main>
+			<h1>
+				Sequência de <a href="https://pt.wikipedia.org/wiki/Sequ%C3%AAncia_de_Fibonacci" target="_blank">Fibonnaci</a>
+			</h1>
+			<div className="form-control">
+				<label htmlFor="fibonacci_limit">Limite da sequência:</label>
+				<input
+					type="number"
+					id="fibonacci_limit"
+					min="0"
+					value={fibonacci}
+					onChange={ev => setFibonacci(parseInt(ev.target.value))}
+				/>
+			</div>
+
+			{fibonacci > 2 ? generateSequenceFibonacci(fibonacci).join(", ") : ""}
+		</main>
+	);
 }
-
-export default App;
